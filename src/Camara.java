05ee5f97fb2cddb50e48eb24c7a30e586ddbfe6d
thomas <t.kinderman01@ufromail.cl>
2023-11-14 19:45:17 -0300

    v2

--- src/Camara.java
+++ src/Camara.java
@@ -1,41 +1,38 @@
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
 public class Camara {
     public static void main(String[] args) {
-        int puerto = 5050;
+        if (args.length != 2) {
+            System.out.println("Uso: java Camara <host> <puerto>");
+            System.exit(1);
+        }
+
+        String host = args[0];
+        int puertoCamara = Integer.parseInt(args[1]);
 
         try {
-            // Crear el socket del servidor
-            ServerSocket servidorSocket = new ServerSocket(puerto);
-            System.out.println("Escuchando en el puerto " + puerto);
-            Socket clienteSocket = servidorSocket.accept();
+            Socket clienteSocket = new Socket(host, puertoCamara);
             OutputStream salida = clienteSocket.getOutputStream();
 
-            int i = 0;
-
-            while(i<100) {
+            while (true) {
                 String enviarPatente = generarPatenteChilena() + "\n";
                 salida.write(enviarPatente.getBytes());
-                i++;
+
+                int tiempoEspera = new Random().nextInt(36000);
+                Thread.sleep(tiempoEspera);
             }
-
-            clienteSocket.close();
-            servidorSocket.close();
-
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static String generarPatenteChilena() {
         char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
-                        'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                        'W', 'X', 'Y', 'Z'};
+                'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z'};
         int[] numeros = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         StringBuilder patente = new StringBuilder();
@@ -53,3 +50,4 @@ public class Camara {
         return patente.toString();
     }
 }
+
--- src/Receptor.java
+++ src/Receptor.java
@@ -1,28 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Receptor {
     public static void main(String[] args) {
-        String host = "localhost";
         int puerto = 5050;
 
         try {
-            // Crear el socket del cliente
-            Socket clienteSocket = new Socket(host, puerto);
+            // Crear el socket del servidor
+            ServerSocket receptorSocket = new ServerSocket(puerto);
+            System.out.println("Escuchando en el puerto " + puerto);
 
+            while (true) {
+                // Esperar a que una cámara se conecte
+                Socket camaraSocket = receptorSocket.accept();
+
+                // Obtener la dirección IP y puerto de la cámara
+                String direccionCamara = camaraSocket.getInetAddress().getHostAddress();
+                int puertoCamara = camaraSocket.getPort();
+                System.out.println("Conexión establecida con la cámara en la dirección: " + direccionCamara +
+                        ", puerto: " + puertoCamara);
+
+                // Manejar la conexión en un nuevo hilo
+                Thread camaraThread = new Thread(new CamaraHandler(camaraSocket, direccionCamara, puertoCamara));
+                camaraThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class CamaraHandler implements Runnable {
+    private Socket camaraSocket;
+    private String direccionCamara;
+    private int puertoCamara;
+
+    public CamaraHandler(Socket camaraSocket, String direccionCamara, int puertoCamara) {
+        this.camaraSocket = camaraSocket;
+        this.direccionCamara = direccionCamara;
+        this.puertoCamara = puertoCamara;
+    }
+
+    @Override
+    public void run() {
+        try {
             // Obtener el stream de entrada después de la conexión
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(camaraSocket.getInputStream()));
 
-            // Leer datos del servidor
+            // Leer datos de la cámara
             String patenteRecibida;
             while ((patenteRecibida = entrada.readLine()) != null) {
-                System.out.println("Patente recibida de una cámara: " + patenteRecibida);
+                System.out.println("Patente recibida de la cámara en " + direccionCamara +
+                        ", puerto " + puertoCamara + ": " + patenteRecibida);
             }
 
             // Cerrar conexión
-            clienteSocket.close();
+            camaraSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
